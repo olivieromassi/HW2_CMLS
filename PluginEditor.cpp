71f3b22ab0ceb3f8aedbc8d6699317eb34123c73
olivieromassi <oliviero.massi@mail.polimi.it>
2020-05-20 19:49:00 +0200

    Added GUI final version

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -23,7 +23,8 @@ Flanger1AudioProcessorEditor::Flanger1AudioProcessorEditor (Flanger1AudioProcess
     labelFont = new Font("Broadway", 20.0f, 1);
     
     // Setting the properties of the LFO rate slider
-    freqLFO_slider.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    freqLFO_slider.setSliderStyle(Slider::Rotary);
+    freqLFO_slider.setLookAndFeel(&otherLookAndFeel);
     freqLFO_slider.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::crimson);
     freqLFO_slider.setSize(100, 100);
     freqLFO_slider.setRange(0.1, 10.0, 0.1);
@@ -44,7 +45,8 @@ Flanger1AudioProcessorEditor::Flanger1AudioProcessorEditor (Flanger1AudioProcess
     freqLFOVal = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.parameters, "FreqLFO", freqLFO_slider);
 
     // Setting the properties of the Depth/Wet slider
-    wet_slider.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    wet_slider.setSliderStyle(Slider::Rotary);
+    wet_slider.setLookAndFeel(&otherLookAndFeel);
     wet_slider.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::crimson);
     wet_slider.setSize(100, 100);
     wet_slider.setRange(0.0, 1.0, 0.01);
@@ -64,7 +66,8 @@ Flanger1AudioProcessorEditor::Flanger1AudioProcessorEditor (Flanger1AudioProcess
     wetVal = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.parameters, "Wet", wet_slider);
 
     // Setting the properties of the Feedback slider
-    feedback_slider.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    feedback_slider.setSliderStyle(Slider::Rotary);
+    feedback_slider.setLookAndFeel(&otherLookAndFeel);
     feedback_slider.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::crimson);
     feedback_slider.setSize(100, 100);
     feedback_slider.setRange(0.0, 1.0, 0.01);
@@ -84,7 +87,8 @@ Flanger1AudioProcessorEditor::Flanger1AudioProcessorEditor (Flanger1AudioProcess
     feedbackVal = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.parameters, "Feedback", feedback_slider);
 
     // Setting the properties of the Width slider
-    width_slider.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    width_slider.setSliderStyle(Slider::Rotary);
+    width_slider.setLookAndFeel(&otherLookAndFeel);
     width_slider.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::crimson);
     width_slider.setSize(100, 100);    
     width_slider.setRange(1.0, 20.0);
@@ -106,11 +110,12 @@ Flanger1AudioProcessorEditor::Flanger1AudioProcessorEditor (Flanger1AudioProcess
 
     // Setting the properties of the Waveform slider
     waveform_slider.setSliderStyle(Slider::LinearVertical);
-    waveform_slider.setSize(100, 100);
+    waveform_slider.setLookAndFeel(&otherLookAndFeel);
+    waveform_slider.setSize(100, 90);
     waveform_slider.setRange(0, 2);
     waveform_slider.setVisible(true);
-    waveform_slider.setTextBoxStyle(Slider::TextBoxBelow, true, 80, 20);
-    waveform_slider.setPopupDisplayEnabled(true, false, this);
+    waveform_slider.setTextBoxStyle(Slider::NoTextBox, true, 80, 20);
+    waveform_slider.setPopupDisplayEnabled(false, false, this);
     waveform_slider.setValue(0);
     addAndMakeVisible(&waveform_slider);
 
@@ -120,18 +125,40 @@ Flanger1AudioProcessorEditor::Flanger1AudioProcessorEditor (Flanger1AudioProcess
     waveform_label.setSize(100, 20);
     addAndMakeVisible(&waveform_label);
 
+    waveformFont = new Font("Broadway", 12.0f, 1);
+    
+    sine_label.setText("Sine", dontSendNotification);
+    sine_label.setFont(*waveformFont);
+    sine_label.setColour(Label::ColourIds::textColourId, Colours::black);
+    triang_label.setText("Triangular", dontSendNotification);
+    triang_label.setFont(*waveformFont);
+    triang_label.setColour(Label::ColourIds::textColourId, Colours::black);
+    saw_label.setText("SawTooth", dontSendNotification);
+    saw_label.setFont(*waveformFont);
+    saw_label.setColour(Label::ColourIds::textColourId, Colours::black);
+    addAndMakeVisible(&sine_label);
+    addAndMakeVisible(&triang_label);
+    addAndMakeVisible(&saw_label);
+
     // Assigning the Slider to the processor parameter
     waveformVal = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.parameters, "Waveform", waveform_slider);
 
     // Choosing the font to be used in the Title label
     pluginNameFont = new Font("Broadway", 40.0f, 1);
 
-    // Setting the properties of the Plugin Title
+    // Setting the properties of the Plugin Name label
     pluginName_label.setText("FlangerONE", dontSendNotification);
     pluginName_label.setSize(500, 40);
     pluginName_label.setColour(Label::ColourIds::textColourId, Colours::black);
     pluginName_label.setFont(*pluginNameFont);
     addAndMakeVisible(&pluginName_label);
+
+    // Setting the properties of the Signature label
+    signature.setFont(*labelFont);
+    signature.setText("Group 15", dontSendNotification);
+    signature.setSize(80, 20);
+    addAndMakeVisible(&signature);
+    
 
     // Setting the properties of the Stereo button
     stereo_button.setSize(20, 20);
@@ -185,25 +212,33 @@ void Flanger1AudioProcessorEditor::resized()
 
     // Placing the Width Slider and its corresponding Label
     width_slider.setBounds(10, getHeight() - 140, 500, getHeight() - 60);
-    width_label.setBounds(24, getHeight() - 160, 80, 20);
+    width_label.setBounds(24, getHeight() - 170, 80, 20);
 
     // Placing the Wet Slider and its corresponding Label
     wet_slider.setBounds(section + 3, getHeight() - 140, 0, getHeight() - 60);
-    wet_label.setBounds(section + 18, getHeight() - 160, 80, 20);
+    wet_label.setBounds(section + 18, getHeight() - 170, 80, 20);
 
     // Placing the Feedback Slider and its corresponding Label
     feedback_slider.setBounds(2 * section + 3, getHeight() - 140, 100, getHeight() - 60);
-    feedback_label.setBounds(2 * section + 6, getHeight() - 160, 80, 20);
+    feedback_label.setBounds(2 * section + 6, getHeight() - 170, 80, 20);
 
     // Placing the FreqLFO Slider and its corresponding Label
     freqLFO_slider.setBounds(3 * section + 3, getHeight() - 140, getWidth() + 100, getHeight() - 60);
-    freqLFO_label.setBounds(3 * section + 15, getHeight() - 160, 80, 20);
+    freqLFO_label.setBounds(3 * section + 15, getHeight() - 170, 80, 20);
 
     // Placing the Waveform Slider and its corresponding Label
-    waveform_slider.setBounds(4 * section + 3, getHeight() - 140, getWidth() + 100, getHeight() - 60);
-    waveform_label.setBounds(4 * section + 6, getHeight() - 160, 80, 20);
+    waveform_slider.setBounds(4 * section - 20, getHeight() - 135, getWidth() + 100, getHeight() - 60);
+    waveform_label.setBounds(4 * section + 6, getHeight() - 170, 80, 20);
 
     // Placing the Stereo Button and its corresponding Label
     stereo_button.setBounds(520, 30, 20, 20);
     stereo_label.setBounds(450, 30, 20, 20);
+
+    // Placing the Signature label
+    signature.setBounds(505, getHeight() - 35, 80, 20);
+
+
+    sine_label.setBounds(getWidth() - 90, getHeight() - 78, 80, 33);
+    triang_label.setBounds(getWidth() - 90, getHeight() - 108, 80, 33);
+    saw_label.setBounds(getWidth() - 90, getHeight() - 137, 80, 33);
 }
--- PluginEditor.h
+++ PluginEditor.h
@@ -16,6 +16,45 @@
 //==============================================================================
 /**
 */
+class OtherLookAndFeel : public LookAndFeel_V4
+{
+public:
+    OtherLookAndFeel()
+    {
+        setColour(Slider::thumbColourId, Colours::black);
+    }
+
+    void drawRotarySlider(Graphics& g, int x, int y, int width, int height, float sliderPos,
+        const float rotaryStartAngle, const float rotaryEndAngle, Slider&) override
+    {
+        auto radius = jmin(width / 2, height / 2) - 4.0f;
+        auto centreX = x + width * 0.5f;
+        auto centreY = y + height * 0.5f;
+        auto rx = centreX - radius;
+        auto ry = centreY - radius;
+        auto rw = radius * 2.0f;
+        auto angle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
+
+        // fill
+        g.setColour(Colour(150, 20, 120));
+        g.fillEllipse(rx, ry, rw, rw);
+
+        // outline
+        g.setColour(Colours::black);
+        g.drawEllipse(rx, ry, rw, rw, 3.0f);
+
+        Path p;
+        auto pointerLength = radius * 0.5f;
+        auto pointerThickness = 4.0f;
+        p.addRectangle(-pointerThickness * 0.5f, -radius, pointerThickness, pointerLength);
+        p.applyTransform(AffineTransform::rotation(angle).translated(centreX, centreY));
+
+        // pointer
+        g.setColour(Colours::black);
+        g.fillPath(p);
+    }
+};
+
 class Flanger1AudioProcessorEditor  : public AudioProcessorEditor
 {
 public:
@@ -41,16 +80,23 @@ public:
     Label waveform_label;
     Label stereo_label;
 
+    Label sine_label;
+    Label triang_label;
+    Label saw_label;
+
     Label pluginName_label;
+    Label signature;
 
     Font* pluginNameFont;
     Font* labelFont;
+    Font* waveformFont;
 
 private:
     
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     Flanger1AudioProcessor& processor;
+    OtherLookAndFeel otherLookAndFeel;
 
 public:
     std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> widthVal;
